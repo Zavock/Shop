79cb1d6b72bfbc63a8feebe9ce39b06bf1377161
Juan Ospina <zuleta2411@hotmail.com>
2023-09-11 14:37:43 -0500

    CheckoutDetail created

---
+++ src/Components/CheckoutDetail/index.jsx
@@ -0,0 +1,25 @@
+import React, {useContext} from 'react';
+import {ShopContext} from '../../Context';
+
+const CheckoutDetail = () => {
+
+  const {isCheckoutDetailOpen, closeCheckoutDetail} = useContext(ShopContext);
+
+  return (
+    <aside className={`${isCheckoutDetailOpen ? 'flex' : 'hidden'} flex-col top-[68px] shadow-2xl fixed right-0 border bg-white border-black rounded-lg w-[360px] h-[calc(100vh-68px)] z-10`}>
+      <div className='flex justify-between items-center p-6'>
+        <h2 className='font-medium text-xl'>Detail</h2>
+        <button onClick={() => closeCheckoutDetail()}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        </button>
+      </div>
+    </aside>
+
+
+  );
+  
+}
+
+export default CheckoutDetail;
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -3,13 +3,22 @@ import React, { createContext, useState } from 'react';
 const ShopContext = createContext();
 
 const ShopProvider = ({children}) => {
-
+  //State of cart
   const [count, setCount] = useState(0);
+  const [shopCart, setShopCart] = useState([]);
+
+  //State of product detail
   const [isProductDetailOpen, setIsProductDetailOpen] = useState(false);
   const openProductDetail = () => setIsProductDetailOpen(true);
   const closeProductDetail = () => setIsProductDetailOpen(false);
+
+  //State of checkout detail
+  const [isCheckoutDetailOpen, setIsCheckoutDetailOpen] = useState(false);
+  const openCheckoutDetail = () => setIsCheckoutDetailOpen(true);
+  const closeCheckoutDetail = () => setIsCheckoutDetailOpen(false);
+
+  //State of product to show in section of product detail
   const [productToShow, setProductToShow] = useState({});
-  const [shopCart, setShopCart] = useState([]);
 
   return (
     <ShopContext.Provider value={{
@@ -18,6 +27,9 @@ const ShopProvider = ({children}) => {
       openProductDetail,
       closeProductDetail,
       isProductDetailOpen,
+      openCheckoutDetail,
+      closeCheckoutDetail,
+      isCheckoutDetailOpen,
       setProductToShow,
       productToShow,
       setShopCart,
